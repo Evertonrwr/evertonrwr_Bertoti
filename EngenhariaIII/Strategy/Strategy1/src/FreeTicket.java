2f5edc7835fbd5b68570cf4e1cab20a6e0428862
Everton Rocha <evertonrwrocha@gmail.com>
2023-03-06 09:43:52 -0300

    Delete EngenhariaIII/Strategy/Strategy1 directory

--- EngenhariaIII/Strategy/Strategy1/src/FreeTicket.java
+++
@@ -1,7 +0,0 @@
-public class FreeTicket implements ConcertTicket {
-    @Override
-    public void buyTicket(int quantity, double price) {
-        System.out.println("Attendant: Here it Goes. Good News, It's a free concert!");
-    }
-
-}
