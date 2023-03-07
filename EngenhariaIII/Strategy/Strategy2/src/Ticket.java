15e50ded9e0719a12ad98345d0a37638d35e9849
Everton Rocha <evertonrwrocha@gmail.com>
2023-03-07 11:53:16 -0300

    Add files via upload

---
+++ EngenhariaIII/Strategy/Strategy2/src/Ticket.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class Ticket{
+    public int Quantity;
+    public Double Price;
+    public ConcertTicket concertTicket;
+    public Ticket(int quantity, double ticketPrice, ConcertTicket concertTicketType){
+        Quantity = quantity;
+        Price = ticketPrice;
+        concertTicket = concertTicketType;
+    }
+
+    public void setConcertTicket(ConcertTicket concertTicket) {
+        this.concertTicket = concertTicket;
+    }
+
+    public void getTicket() {
+        this.concertTicket.buyTicket(this.Quantity, this.Price);
+    }
+}
